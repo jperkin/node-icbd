f985f946b382016617aa5232861aa3fbebeb7e1a
Jonathan Perkin <jonathan@perkin.org.uk>
2012-02-20 12:28:16 +0000

    Add a very basic client which just logs in and dumps messages.
    Useful for testing, eventually will probably want to abstract the
    ICB layer properly so we can write both servers and clients easily.

---
+++ icb.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+/*
+ * Really dumb ICB client, currently just used for testing.
+ */
+var net = require('net');
+
+var client = function() {
+  var socket = net.createConnection(7326, process.ARGV[2]);
+
+  socket.setEncoding("utf8");
+
+  socket.on('connect', function(connect) {
+    sendicbmsg("ajperkin\001jperkin@icbjs\001test\001login");
+  });
+
+  socket.on('data', function(data) {
+    data.split("\0").forEach(function(line) {
+      console.log(line.replace(/\001/g, "^A").slice(1));
+    });
+  });
+
+  function sendicbmsg(message) {
+    socket.write(String.fromCharCode(message.length) + message);
+  }
+}
+
+if (!process.ARGV[2]) {
+  console.log("usage: icb.js <server>");
+  process.exit(2);
+}
+client();
